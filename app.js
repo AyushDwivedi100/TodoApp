9e61dd46d28c83c4b7c2650f8103bcd7a0b6b806
Ayush Dwivedi <ayushd100@gmail.com>
2025-06-15 09:17:38 +0530

    Refactor Todo App to implement object structure for todos, enhance localStorage handling, and update UI interactions

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const Inputform = document.querySelector("form");
 const toDoUl = document.getElementById("todoul");
 const InputValue = document.getElementById("write-text-box");
 
-const toDoList = [];
+const toDoList = getItem();
+updateToDoList();
 
 Inputform.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -12,8 +13,13 @@ Inputform.addEventListener("submit", function (e) {
 function updateToDoList() {
   const toDoValue = InputValue.value.trim();
   if (toDoValue != "") {
-    toDoList.push(toDoValue);
+    const todoObject = {
+      todoText: toDoValue,
+      completed: false,
+    };
+    toDoList.push(todoObject);
   }
+  setItem();
   InputValue.value = "";
   appendUl();
 }
@@ -21,8 +27,10 @@ function updateToDoList() {
 function appendUl() {
   toDoUl.innerHTML = "";
   toDoList.forEach((toDo, toDoIndex) => {
-    toDoItem = createToDo(toDo, toDoIndex);
+    toDoItem = createToDo(toDo.todoText, toDoIndex);
     toDoUl.append(toDoItem);
+    const checkbox = document.getElementById("text-box-" + `${toDoIndex}`);
+    checkbox.checked = toDoList[toDoIndex].completed;
   });
 }
 
@@ -35,7 +43,7 @@ function createToDo(toDo, toDoIndex) {
   const Indexing = "text-box-" + toDoIndex;
   const toDoLi = document.createElement("li");
   toDoLi.innerHTML = `
-            <input type="checkbox" class="text-box" id="${Indexing}" />
+            <input type="checkbox" class="text-box" id="${Indexing}" onclick="checkingUpdate(${toDoIndex})">
             <label for="${Indexing}" class="toDoCheck">
               <svg
                 fill="transparent"
@@ -68,4 +76,19 @@ function createToDo(toDo, toDoIndex) {
             </button>
           `;
   return toDoLi;
+}
+
+function setItem() {
+  const JSONSet = JSON.stringify(toDoList);
+  localStorage.setItem("todos", JSONSet);
+}
+
+function getItem() {
+  const JSONGet = localStorage.getItem("todos") || "[]";
+  return JSON.parse(JSONGet);
+}
+
+function checkingUpdate(todoIndex) {
+  toDoList[todoIndex].completed = true;
+  setItem();
 }
